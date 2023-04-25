8dbcc89d23699d220bd3f26be12d3b9fe3f42f60
Stefan Jipescu <stefanjipescu@gmail.com>
2023-04-26 01:14:30 +0300

    --

--- database.py
+++ database.py
@@ -12,13 +12,13 @@ class DatabaseManager:
         self.connection.close()
     
     def _execute(self, statement: str, values: t.Optional[t.Tuple[str]] = None) -> sqlite3.Cursor:
-        """Take a  statement and execute it with SQLite"""
+        """Take a  statement and optionally values for the placeholders, in order to execute it with SQLite"""
         try:
             with self.connection:
                 cursor = self.connection.cursor()
                 cursor.execute(statement, values or [])
                 return cursor
-        except sqlite3.IntegrityError:
+        except (sqlite3.IntegrityError, sqlite3.OperationalError, sqlite3.ProgrammingError):
             print(f'Something went wrong with the following transaction: \n{statement}')
             raise
     
@@ -45,11 +45,57 @@ class DatabaseManager:
         the columns and the value is the value 
         """
         columns_name = ", ".join(data.keys())
-        placeholders = ", ".join(["?" * len(data.keys())])
+        placeholders = ", ".join(["?"] * len(data.keys()))
         columns_values = tuple(data.values())
         query = f"""
             INSERT INTO
-                {table_name} ({columns_name}) VALUES(
+                {table_name} (
+                    {columns_name}
+                ) VALUES (
                     {placeholders}
                 );
-                """
+        """
+        print(query)
+        self._execute(query, columns_values)
+    
+    def delete(self, table_name: str, criteria: t.Dict[str, str]):
+        '''Take a table name and delete the information based on the provided criteria'''
+        placeholders = [f'{column} = ?' for column in criteria.keys()]
+        delete_criteria = ' AND '.join(placeholders)
+        delete_criteria_values = tuple(criteria.values())
+        query = f'''
+            DELETE FROM 
+                {table_name}
+            WHERE
+                {delete_criteria};
+        '''
+        self._execute(query, delete_criteria_values)
+    
+    def select(self,
+        table_name: str,
+        columns: list = None,
+        criteria : t.Dict[str, str] = {}, 
+        order_by: t.Optional[str ] = None,
+        ordered_descending: bool = False 
+        ) -> sqlite3.Cursor:
+        '''
+        Takes in a table name and optionally a criteria as a dictionary, a column to order by and a boolean 
+        flag to order it by that column descending or not 
+        '''
+
+        select_criteria_values = tuple(criteria.values())
+        if columns:
+            select_columns = ', '.join(columns)
+            query = f'SELECT {select_columns} FROM {table_name}'
+        else:
+            query = f'SELECT * from {table_name}'
+        if criteria:
+            placeholders = [f'{column} = ?' for column in criteria.keys()]
+            select_criteria = " AND ".join(placeholders)
+            query = query + f' WHERE {select_criteria}'
+        if order_by:
+            query = query + f' ORDER BY {order_by}'
+            if ordered_descending:
+                query = query + ' DESC'
+        query = query + ';'
+        return self._execute(query, select_criteria_values)
